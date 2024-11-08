cfbca57ea3a9b4eba3c0a3f9f8683543f7131978
Rodrigo Licks <rodrigo.licks@gmail.com>
2024-11-08 18:04:05 -0300

    Validation fix

--- src/main/java/br/net/licks/db/model/Associado.java
+++ src/main/java/br/net/licks/db/model/Associado.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Entity
@@ -12,6 +13,7 @@ public class Associado {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;	
+	private Integer id;
+	@NotEmpty
 	private String nome;
 }
